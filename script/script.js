d4bcbbd4acc9d72847d481596e29d3d38ee7c1b9
Virgil <vvilcu489@gmail.com>
2022-11-09 23:35:37 +0200

    created all files

---
+++ script/script.js
@@ -0,0 +1,18 @@
+// const card1 = document.querySelectorAll('.card1');
+//
+//
+// card1.forEach(item => {
+//     item.addEventListener('mouseenter', (ev) => {
+//             const getDistanceY = () => {
+//                 return window.parseInt(item.style.transform.replace('translateY(', ''), 10) || 0;
+//             }
+//             let disY = getDistanceY() - 11 ;
+//
+//             item.style.transform = 'translateY(' + disY + 'vw)';
+//             item.style.transitionDelay = '1s'
+//
+//         }
+//     )
+// })
+//
+//
